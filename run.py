8941d4a2a17bde4a01a46d9cdd46b2964349f97b
Dombearx <domin.dober@gmail.com>
2020-04-29 00:19:58 +0200

    merge with run_full

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from scipy import misc
+from scipy import misc, ndimage
 import glob
 from skimage import io
 from skimage.transform import resize
@@ -109,7 +109,56 @@ def calculate_similarity(image1, image2):
     
     return percentage
 
+def simple_find_contours(images):
+    pixelContours = []
+    neighborhood = [(-1,-1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    for image in images:
+        pixC = []
+        for row, line in enumerate(image):
+            for column, pixel in enumerate(line):
+                if(pixel == 0): # jeśli tło (czarne), to opuść tę iterację
+                    continue
+                for x,y in neighborhood: # jeśli pixel figury (biały), to przejrzyj jego siąsiadów
+                    try:
+                        if image[row + x, column + y] == 0: # if sąsiad pixela jest tłem (czarny), to pixel (jego współrzędne) dodaj do listy krawędzi
+                            pixC.append((row, column))
+                            break
+                    except:
+                        print("err")
+        pixelContours.append(pixC) # dodaj cały kontur (ciąg pixeli) danego obrazka do listy konturów
+    return pixelContours
 
+
+def simple_find_contours(image):
+    neighborhood = [(-1,-1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+
+    pixC = []
+    for row, line in enumerate(image):
+        for column, pixel in enumerate(line):
+            if(pixel == 0): # jeśli tło (czarne), to opuść tę iterację
+                continue
+            for x,y in neighborhood: # jeśli pixel figury (biały), to przejrzyj jego siąsiadów
+                try:
+                    if image[row + x, column + y] == 0: # if sąsiad pixela jest tłem (czarny), to pixel (jego współrzędne) dodaj do listy krawędzi
+                        pixC.append((row, column))
+                        break
+                except:
+                    print("err")
+    return pixC
+
+def hough_rotate_image(image, contursMap):
+    angles = []
+    lines = cv2.HoughLinesP(contursMap, 1, math.pi / 180.0, 50, minLineLength=50, maxLineGap=5)
+    for x1, y1, x2, y2 in lines[0]:
+        #cv2.line(contursMap, (x1, y1), (x2, y2), (255, 0, 0), 3) <- not needed
+        angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
+        angles.append(angle)
+        
+    median_angle = np.median(angles)
+    # RYSOWANIE
+    # axs[math.floor(index / 2),index % 2].imshow(imgs_rotated[-1], cmap='gray', vmin=0, vmax=255) 
+    # index += 1
+    return ndimage.rotate(image, median_angle)
 
 if __name__ == "__main__":
     images = []
@@ -135,27 +184,16 @@ if __name__ == "__main__":
 
     contours = []
     contursMaps = []
-    for image in images:    
-        contours.append([(r,c) for r,c in np.floor(find_contours(image,0.5)[0])]) 
+    for image in images:     
         contursMaps.append(np.zeros(image.shape, dtype="uint8"))
 
     pixelContours = []
-    neighborhood = [(-1,-1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-    for image in images:
-        pixC = []
-        for row, line in enumerate(image):
-            for column, pixel in enumerate(line):
-                if(pixel == 0): # jeśli tło (czarne), to opuść tę iterację
-                    continue
-                for x,y in neighborhood: # jeśli pixel figury (biały), to przejrzyj jego siąsiadów
-                    try:
-                        if image[row + x, column + y] == 0: # if sąsiad pixela jest tłem (czarny), to pixel (jego współrzędne) dodaj do listy krawędzi
-                            pixC.append((row, column))
-                            break
-                    except:
-                        print("err")
-        pixelContours.append(pixC) # dodaj cały kontur (ciąg pixeli) danego obrazka do listy konturów
 
+    for image in images:
+        try:
+            pixelContours.append(simple_find_contours(image)) # dodaj cały kontur (ciąg pixeli) danego obrazka do listy konturów
+        except:
+            pixelContours.append(image)
       
     for img_id, contour in enumerate(pixelContours): #kontur jednego obrazka
         for contour_pixel in contour: #jeden pixel (x,y) w konturze
@@ -173,28 +211,40 @@ if __name__ == "__main__":
 
     # RYSOWANIE
     # fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
-    from scipy import ndimage
-    index = 0
     imgs_rotated = []
     for image, contursMap in zip(images, contursMaps):
-        angles = []
-        lines = cv2.HoughLinesP(contursMap, 1, math.pi / 180.0, 50, minLineLength=50, maxLineGap=5)
-        for x1, y1, x2, y2 in lines[0]:
-            cv2.line(contursMap, (x1, y1), (x2, y2), (255, 0, 0), 3)
-            angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
-            angles.append(angle)
-            
-        median_angle = np.median(angles)
-        imgs_rotated.append(ndimage.rotate(image, median_angle))
-        # axs[math.floor(index / 2),index % 2].imshow(imgs_rotated[-1], cmap='gray', vmin=0, vmax=255) 
-        index += 1
-    # plt.show()
+        try:
+            imgs_rotated.append(hough_rotate_image(image, contursMap))
+        except:
+            imgs_rotated.append(image)
+
+    del contursMaps
+    del images
 
 
-    ready_images = [] 
+    removed_background = []
     for image in imgs_rotated:
-        ready_images.append( cut_white_block( check_orient( cut_background(image) ) ) )  # check_orient + cut_background + cut_white_block w jednej pętli
+        try:
+            removed_background.append(cut_background(image))
+        except:
+            removed_background.append(image)
+    del imgs_rotated
 
+
+    imgs_fliped = [] 
+    for image in removed_background:
+        try:
+            imgs_fliped.append(check_orient(image))  
+        except:
+            imgs_fliped.append(image) 
+    del removed_background
+
+    ready_images = []
+    for image in imgs_fliped:
+        try:
+            ready_images.append(cut_white_block(image))
+        except:
+            ready_images.append(image)
 
     # RYSOWANIE
     # fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
@@ -212,10 +262,19 @@ if __name__ == "__main__":
         if w < minWidth:
             minWidth = w
 
+    if(minHeight * 6 < minWidth):
+        minHeight = int(minWidth / 6) #Zapobiega przeskalowaniu wszystkich do bardzo małego prostokąta jeżeli któraś figura była prostokątem i usuwanie białego bloku wycięło praktycznie całość
     resized_images = []
-    for image in ready_images:
-        resized_images.append(cv2.resize(image, dsize=(minWidth, minHeight), interpolation=cv2.INTER_CUBIC))
+    for index, image in enumerate(ready_images):
+        h,w = image.shape
+        
+        if h >= minHeight and w >= minWidth:
+            resized_images.append(cv2.resize(image, dsize=(minWidth, minHeight), interpolation=cv2.INTER_CUBIC))
+        else:
+            resized_images.append(cv2.resize(imgs_fliped[index], dsize=(minWidth, minHeight), interpolation=cv2.INTER_CUBIC))
 
+    del ready_images
+    del imgs_fliped
     # RYSOWANIE
     # fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
     # index = 0
@@ -237,7 +296,7 @@ if __name__ == "__main__":
                         
         similars.sort(key=lambda x: x[1], reverse=True)
 
-        # string_for_file = str(index)+" :"
+        string_for_file = str(index)+" :"
         string = ""
         for i in similars:
             string += " "+str(i[0])
