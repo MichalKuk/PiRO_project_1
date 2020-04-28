ffa8fb39068e052c5f7c107d8b14b2cb4c562b00
MichalKuk <michal.kukiela123@gmail.com>
2020-04-28 21:33:18 +0200

    working run.py

--- run.py
+++ run.py
@@ -111,161 +111,164 @@ def calculate_similarity(image1, image2):
 
 
 
-images = []
+if __name__ == "__main__":
+    images = []
 
-for i in range(sys.argv[2]): 
-    # path = Path(str(sys.argv[1])+'\\'+str(i)+'.png)
-    path = os.path.join(str(sys.argv[1]), str(i), '.png')
-    image = io.imread(path, as_gray=True)
-    images.append((int(i), image)) 
+    # path = sys.argv[1]
+    # print(path)
+    # path = os.path.abspath(sys.argv[1])
+    # print(path)
 
-images.sort(key=lambda x: x[0])
-images = [img for _, img in images] # zostawia images bez indeksów
+    for i in range(int(sys.argv[2])): 
+        # path = Path(str(sys.argv[1])+'\\'+str(i)+'.png)
+        p = os.path.abspath(sys.argv[1])
+        path = os.path.join(p, str(i)+'.png')
+        image = io.imread(path, as_gray=True)
+        images.append((int(i), image)) 
 
-# RYSOWANIE
-# fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
-# for index, image in enumerate(images):
-#     axs[math.floor(index / 2),index % 2].imshow(image, cmap='gray', vmin=0, vmax=255) 
+    images.sort(key=lambda x: x[0])
+    images = [img for _, img in images] # zostawia images bez indeksów
 
-# numpyImages = []
-# for image in images:
-#     numpyImages.append(np.array(image))
-# 
-# contours = []
-# for image in images:    
-#     contours.append(find_contours(image,0.5))
-# 
-# contours_floors = []
-# for cont in contours:
-#     contours_floors.append([(r,c) for r,c in np.floor(cont[0])])
-
-contours = []
-contursMaps = []
-for image in images:    
-    contours.append([(r,c) for r,c in np.floor(find_contours(image,0.5)[0])]) 
-    contursMaps.append(np.zeros(image.shape, dtype="uint8"))
-
-# contursMaps = []
-# for image in images:
-#     contursMaps.append(np.zeros(image.shape, dtype="uint8"))
-
-
-pixelContours = []
-neighborhood = [(-1,-1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-for image in images:
-    pixC = []
-    for row, line in enumerate(image):
-        for column, pixel in enumerate(line):
-            if(pixel == 0): # jeśli tło (czarne), to opuść tę iterację
-                continue
-            for x,y in neighborhood: # jeśli pixel figury (biały), to przejrzyj jego siąsiadów
-                try:
-                    if image[row + x, column + y] == 0: # if sąsiad pixela jest tłem (czarny), to pixel (jego współrzędne) dodaj do listy krawędzi
-                        pixC.append((row, column))
-                        break
-                except:
-                    print("err")
-    pixelContours.append(pixC) # dodaj cały kontur (ciąg pixeli) danego obrazka do listy konturów
-
-
-# for index, image in enumerate(images):
-#     for r, line in enumerate(contursMaps[index]):
-#         for c, _ in enumerate(line):
-#             if((r,c) in pixelContours[index]):
-#                 contursMaps[index][r,c] = 1         
-for img_id, contour in enumerate(pixelContours): #kontur jednego obrazka
-    for contour_pixel in contour: #jeden pixel (x,y) w konturze
-        contursMaps[img_id][contour_pixel[0]][contour_pixel[1]] = 1 # zamaluj na biało pixel odpowiadający pixelowi konturu
-
-
-# RYSOWANIE
-# fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
-# index = 0
-# for image in contursMaps:
-#     axs[math.floor(index / 2),index % 2].imshow(image, cmap='gray', vmin=0, vmax=1) 
-#     index += 1
-
-
-# RYSOWANIE
-# fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
-from scipy import ndimage
-# index = 0
-imgs_rotated = []
-for image, contursMap in zip(images, contursMaps):
-    angles = []
-    lines = cv2.HoughLinesP(contursMap, 1, math.pi / 180.0, 50, minLineLength=50, maxLineGap=5)
-    for x1, y1, x2, y2 in lines[0]:
-        cv2.line(contursMap, (x1, y1), (x2, y2), (255, 0, 0), 3)
-        angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
-        angles.append(angle)
-        
-    median_angle = np.median(angles)
-    imgs_rotated.append(ndimage.rotate(image, median_angle))
     # RYSOWANIE
-    # axs[math.floor(index / 2),index % 2].imshow(imgs_rotated[-1], cmap='gray', vmin=0, vmax=255) 
-    # index += 1
+    # fig, axs = plt.subplots(math.ceil(len(images)/2),2, figsize=(12,12))
+    # for index, image in enumerate(images):
+    #     axs[math.floor(index / 2), index % 2].imshow(image, cmap='gray', vmin=0, vmax=255) 
+    # plt.show()
+
+    contours = []
+    contursMaps = []
+    for image in images:    
+        contours.append([(r,c) for r,c in np.floor(find_contours(image,0.5)[0])]) 
+        contursMaps.append(np.zeros(image.shape, dtype="uint8"))
+
+    pixelContours = []
+    neighborhood = [(-1,-1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    for image in images:
+        pixC = []
+        for row, line in enumerate(image):
+            for column, pixel in enumerate(line):
+                if(pixel == 0): # jeśli tło (czarne), to opuść tę iterację
+                    continue
+                for x,y in neighborhood: # jeśli pixel figury (biały), to przejrzyj jego siąsiadów
+                    try:
+                        if image[row + x, column + y] == 0: # if sąsiad pixela jest tłem (czarny), to pixel (jego współrzędne) dodaj do listy krawędzi
+                            pixC.append((row, column))
+                            break
+                    except:
+                        print("err")
+        pixelContours.append(pixC) # dodaj cały kontur (ciąg pixeli) danego obrazka do listy konturów
+
+      
+    for img_id, contour in enumerate(pixelContours): #kontur jednego obrazka
+        for contour_pixel in contour: #jeden pixel (x,y) w konturze
+            contursMaps[img_id][contour_pixel[0]][contour_pixel[1]] = 1 # zamaluj na biało pixel odpowiadający pixelowi konturu
 
 
-# fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
-# index = 0
-# imgs_fliped = []
-# for image in imgs_rotated:
-#     imgs_fliped.append(check_orient(image))   
-    # axs[math.floor(index / 2),index % 2].imshow(imgs_fliped[-1], cmap='gray', vmin=0, vmax=255) 
-    # index += 1 
-
-# fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
-# index = 0
-# cutted_images = []
-# for image in imgs_fliped:
-#     cutted_images.append(cut_background(image))
-    # axs[math.floor(index / 2),index % 2].imshow(cutted_images[-1], cmap='gray', vmin=0, vmax=255) 
-    # index += 1 
-
-# fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
-# index = 0
-# ready_images = []
-# for image in cut_images:
-#     ready_images.append(cut_white_block(image))     
-    # axs[math.floor(index / 2),index % 2].imshow(ready_images[-1], cmap='gray', vmin=0, vmax=255) 
-    # index += 1
-
-ready_images = [] 
-for image in imgs_rotated:
-    ready_images.append( cut_white_block( cut_background( check_orient(image) ) ) )  # check_orient + cut_background + cut_white_block w jednej pętli
+    # RYSOWANIE
+    # fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
+    # index = 0
+    # for image in contursMaps:
+    #     axs[math.floor(index / 2),index % 2].imshow(image, cmap='gray', vmin=0, vmax=1) 
+    #     index += 1
+    # plt.show()
 
 
-minHeight, minWidth = ready_images[0].shape
-for image in ready_images:
-    h,w = image.shape
-    if h < minHeight:
-        minHeight = h 
-    if w < minWidth:
-        minWidth = w
+    # RYSOWANIE
+    # fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
+    from scipy import ndimage
+    index = 0
+    imgs_rotated = []
+    for image, contursMap in zip(images, contursMaps):
+        angles = []
+        lines = cv2.HoughLinesP(contursMap, 1, math.pi / 180.0, 50, minLineLength=50, maxLineGap=5)
+        for x1, y1, x2, y2 in lines[0]:
+            cv2.line(contursMap, (x1, y1), (x2, y2), (255, 0, 0), 3)
+            angle = math.degrees(math.atan2(y2 - y1, x2 - x1))
+            angles.append(angle)
+            
+        median_angle = np.median(angles)
+        imgs_rotated.append(ndimage.rotate(image, median_angle))
+        # axs[math.floor(index / 2),index % 2].imshow(imgs_rotated[-1], cmap='gray', vmin=0, vmax=255) 
+        index += 1
+    # plt.show()
 
-resized_images = []
-for image in ready_images:
-    resized_images.append(cv2.resize(image, dsize=(minWidth, minHeight), interpolation=cv2.INTER_CUBIC))
 
-# RYSOWANIE
-# fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
-# index = 0
-# for image in resized_images:
-#     axs[math.floor(index / 2),index % 2].imshow(image, cmap='gray', vmin=0, vmax=255) 
-#     index += 1 
+    # # fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
+    # # index = 0
+    # # imgs_fliped = []
+    # # for image in imgs_rotated:
+    # #     imgs_fliped.append(check_orient(image))   
+    #     # axs[math.floor(index / 2),index % 2].imshow(imgs_fliped[-1], cmap='gray', vmin=0, vmax=255) 
+    #     # index += 1 
+
+    # # fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
+    # # index = 0
+    # # cutted_images = []
+    # # for image in imgs_fliped:
+    # #     cutted_images.append(cut_background(image))
+    #     # axs[math.floor(index / 2),index % 2].imshow(cutted_images[-1], cmap='gray', vmin=0, vmax=255) 
+    #     # index += 1 
+
+    # # fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
+    # # index = 0
+    # # ready_images = []
+    # # for image in cut_images:
+    # #     ready_images.append(cut_white_block(image))     
+    #     # axs[math.floor(index / 2),index % 2].imshow(ready_images[-1], cmap='gray', vmin=0, vmax=255) 
+    #     # index += 1
+
+    ready_images = [] 
+    for image in imgs_rotated:
+        ready_images.append( cut_white_block( check_orient( cut_background(image) ) ) )  # check_orient + cut_background + cut_white_block w jednej pętli
 
 
-for index, image in enumerate(resized_images):
-    similars = []
-    for index2, image2 in enumerate(resized_images):
-        if index == index2:
-            continue
-        sim = calculate_similarity(np.flip(255 - image),image2)
-        similars.append((index2, sim))
-                       
-    similars.sort(key=lambda x: x[1], reverse=True)
-    # string = str(index)+" :"
-    string = ""
-    for i in similars:
-        string += " "+str(i[0])
-    print(string)
+    # RYSOWANIE
+    # fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
+    # index = 0
+    # for image in ready_images:
+    #     axs[math.floor(index / 2),index % 2].imshow(image, cmap='gray', vmin=0, vmax=1) 
+    #     index += 1
+    # plt.show()
+
+    minHeight, minWidth = ready_images[0].shape
+    for image in ready_images:
+        h,w = image.shape
+        if h < minHeight:
+            minHeight = h 
+        if w < minWidth:
+            minWidth = w
+
+    resized_images = []
+    for image in ready_images:
+        resized_images.append(cv2.resize(image, dsize=(minWidth, minHeight), interpolation=cv2.INTER_CUBIC))
+
+    # RYSOWANIE
+    # fig, axs = plt.subplots(int(len(images)/2),2, figsize=(12,12))
+    # index = 0
+    # for image in resized_images:
+    #     axs[math.floor(index / 2),index % 2].imshow(image, cmap='gray', vmin=0, vmax=255) 
+    #     index += 1 
+    # plt.show()
+
+
+    # output_file = open("output.txt","w")
+
+    for index, image in enumerate(resized_images):
+        similars = []
+        for index2, image2 in enumerate(resized_images):
+            if index == index2:
+                continue
+            sim = calculate_similarity(np.flip(255 - image),image2)
+            similars.append((index2, sim))
+                        
+        similars.sort(key=lambda x: x[1], reverse=True)
+        
+        # string_for_file = str(index)+" :"
+        string = ""
+        for i in similars:
+            string += " "+str(i[0])
+            string_for_file += " "+str(i[0])
+        print(string)
+        # print(string_for_file, file=output_file)
+
+    # output_file.close()
